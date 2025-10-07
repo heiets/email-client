04c799581c1d5b4fcb2544ac5534749e9be93358
Oleksandr Heiets <o.heiets@gmail.com>
2025-10-07 19:37:49 +0200

    Fixed path format

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.js'
 
 async function enableMocking() {
   const { worker } = await import('./mocks/browser.js')
